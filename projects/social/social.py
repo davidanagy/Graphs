4a940e5a8fe17b46edcc58da0fbdf093b6aae308
davidanagy <davidanagy@users.noreply.github.com>
2020-04-14 15:45:45 -0700

    finished one stretch goal in social.py

--- projects/social/social.py
+++ projects/social/social.py
@@ -48,7 +48,7 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
+        # !!!! IMPLEMENT ME 
 
         # Add users
         for i in range(num_users):
@@ -98,6 +98,39 @@ class SocialGraph:
         return visited
 
 
+"""
+Answers to questions--
+
+1. QUESTION: To create 100 users with an average of 10 friends each,
+how many times would you need to call add_friendship()? Why?
+
+ANSWER: You would need to call it 500 times. In order to give each user
+10 friends on average, there need to be 1000 friendships total (1000 / 100 = 10).
+However, add_friendship() adds *TWO* friendships each time it's called, since it sets
+User B as User A's friend *and also* sets User A as User B's friend. So to get 1000
+friendships, we call add_friendship() 500 times.
+
+2. If you create 1000 users with an average of 5 random friends each,
+what percentage of other users will be in a particular user's extended social network?
+What is the average degree of separation between a user and those in his/her extended network?
+
+Running the code below, I find that the percentage of other users in a social network is about 98.8%,
+and the average degree of separation is about 5.46.
+
+3. You might have found the results from question #2 above to be surprising.
+Would you expect results like this in real life? If not, what are some ways
+you could improve your friendship distribution model for more realistic results?
+
+I wouldn't expect this code in real life. In real life, I assume friendship networks are more
+self-contained, so the percentage of other users in one's extended network would be much lower.
+(The average degree of separation is less intuitive to me.)
+
+For more realistic results, one method is to pre-divide the ID numbers into groups, and only create
+friendships between user IDs in the same group. Or we could keep track of how many users are in
+each person's extended network as we add friendships, and refuse to add any more friendships for
+that user once they hit a certain number. (This number can be varied from user to user.)
+"""
+
 if __name__ == '__main__':
     sg = SocialGraph()
     sg.populate_graph(10, 2)
@@ -106,3 +139,27 @@ if __name__ == '__main__':
     #print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+
+    # Code to answer question #2 above
+    friends_means = []
+    degree_of_sep_means = []
+    for _ in range(10):
+        friends_sum = 0
+        degree_of_sep_sum = 0
+        degree_of_sep_length = 0
+        sg.populate_graph(1000, 5)
+        for user in range(1, 1001):
+            network = sg.get_all_social_paths(user)
+            friends_sum += len(network)
+            for friend in network.keys():
+                degree_of_sep_sum += len(network[friend])
+                degree_of_sep_length += 1
+        
+        friends_means.append(friends_sum / 1000)
+        degree_of_sep_means.append(degree_of_sep_sum / degree_of_sep_length)
+
+    avg_friends = sum(friends_means) / 10
+    avg_degree_of_sep = sum(degree_of_sep_means) / 10
+    print(f'The percentage of other users in the social network is {avg_friends / 10}%.')
+    print(f'The average degree of separation is {avg_degree_of_sep}.')
+    
